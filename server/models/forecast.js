c1b27ad03c1508f7b20d4d2fc62d80cea576518e
Mark Marsella <mmarsella114@g.rwu.edu>
2016-01-16 22:43:16 -0800

    forecast schema

--- server/models/forecast.js
+++ server/models/forecast.js
@@ -15,21 +15,32 @@ var mongoose = require("mongoose");
 /****** FORECAST SCHEMA *******/
 
 var forecastSchema = new mongoose.Schema({
-  spot_name:String, // "South Ocean Beach"
-  date:String,   // ex "2015-9-30 13"
-  hour:String,   //ex 9AM
-  size_ft:Number,  //ex 1.6757142548640278
-  shape:String,  //"pf"
-  tide:String,   //"Poor-fair"
-
-  wind_speed:Number, // http://api.spitcast.com/api/county/wind/san-francisco/?dcat=week
-  direction_degrees:Number,
-  direction_text:String,
-  // time:String,
-  // location:{
-  //   latitude: Number,
-  //   longitude: Number
-  // }
+  localTimestamp: Number,
+  fadedRating: Number,
+  solidRating: Number,
+  absMinBreakingHeight: Number,
+  absMaxBreakingHeight: Number,
+  wind: {
+    speed, String,
+    direction: String,
+    compassDirection: String,
+    chill: String,
+    gusts: String
+  },
+  condition: {
+    pressure: String,
+    temperature: Number,
+    weather: Number,
+    unitPressure: String,
+    unit: String
+  },
+  charts: {
+    swell: String,
+    period: String,
+    wind: String,
+    pressure: String,
+    sst: String
+  }
 });
 
 
