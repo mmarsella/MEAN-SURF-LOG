8bcdd9c9d835a0d6f0cdb5db22d6ef7eda15229a
Mark Marsella <mmarsella114@g.rwu.edu>
2016-01-21 15:53:06 -0800

    deploying to heroku

--- server/app.js
+++ server/app.js
@@ -112,6 +112,6 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is listening on port 3000");
 });
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/surfLog");
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/surfLog");
 mongoose.set("debug",true);
 
 module.exports.User = require("./user");
