758899da81a5e56d4694569da4ea3338151d720f
Mark Marsella <mmarsella114@g.rwu.edu>
2016-01-25 20:24:29 -0800

    style hours and days

--- server/app.js
+++ server/app.js
@@ -65,57 +65,57 @@ function spotName(spot_id){
 // db.forecasts.findOne({spot_id:"4221",numDate:"23",localTimestamp:1453539600})
 
 
-var CronJob = require('cron').CronJob;
-new CronJob('0 */1 * * * *', function() {  
-var d = new Date(); 
-console.log("The time is...",d); 
-  // iter. thru all spots and create a forecast
-  for(var i=0; i < spot_ids.length; i++){
-    // close function (j) so I can immediately invoke it afterwards with i. Great example of scope and closure.
-    (function(j){
-      request.get("http://magicseaweed.com/api/"+process.env.MSW_KEY+"/forecast/?spot_id=" + spot_ids[j], function(err,resp,body){
+// var CronJob = require('cron').CronJob;
+// new CronJob('0 */1 * * * *', function() {  
+// var d = new Date(); 
+// console.log("The time is...",d); 
+//   // iter. thru all spots and create a forecast
+//   for(var i=0; i < spot_ids.length; i++){
+//     // close function (j) so I can immediately invoke it afterwards with i. Great example of scope and closure.
+//     (function(j){
+//       request.get("http://magicseaweed.com/api/"+process.env.MSW_KEY+"/forecast/?spot_id=" + spot_ids[j], function(err,resp,body){
           
 
 
-          var response = JSON.parse(body);
-          console.log("Doing forecast... ");
+//           var response = JSON.parse(body);
+//           console.log("Doing forecast... ");
           
-          response.forEach(function(el){
-            var d = new Date(el.timestamp * 1000);
+//           response.forEach(function(el){
+//             var d = new Date(el.timestamp * 1000);
 
-            // db.Forecast.findOrCreate
-            // If same spot, date and time{  skip storing it into the database!  }
-            // eval(locus)
-            db.Forecast.findOne({spot_id:spot_ids[j], localTimestamp: el.localTimestamp, numDate: d.getDate()}, function(err, found){
-              console.log("INSIDE FIND ONE!", found);
-              if(!found){
-                  console.log("EL", el);
-                  var forecast = new db.Forecast(el);
-                  console.log("D",d);
-                  forecast.year = d.getYear();
-                  forecast.hour = d.getHours();
-                  forecast.numDate = d.getDate();
-                  forecast.numMonth = d.getMonth();
-                  forecast.spot_id = spot_ids[j];
-                  forecast.spot_name = spotName(spot_ids[j]);
-                  forecast.date = new Date(el.timestamp * 1000);
-                  forecast.save(function(err){
-                  if(err){
-                    console.log("ERROR IN SAVING!",err);
-                  }else{
-                    console.log("Succesfully SAVED");
-                  }
-                }); //end Save
-                console.log("SAVED");
-            }else{
-              console.log("IN THE DB!!!  SKIPPED!")
-            }
-           }); //end findOne check
-         }) // end responseForEach
-       })  // END API REQUEST
-      })(i);  // end j
-     }  // end For-loop    
-}, null, true, 'America/Los_Angeles');   //end cron-job
+//             // db.Forecast.findOrCreate
+//             // If same spot, date and time{  skip storing it into the database!  }
+//             // eval(locus)
+//             db.Forecast.findOne({spot_id:spot_ids[j], localTimestamp: el.localTimestamp, numDate: d.getDate()}, function(err, found){
+//               console.log("INSIDE FIND ONE!", found);
+//               if(!found){
+//                   console.log("EL", el);
+//                   var forecast = new db.Forecast(el);
+//                   console.log("D",d);
+//                   forecast.year = d.getYear();
+//                   forecast.hour = d.getHours();
+//                   forecast.numDate = d.getDate();
+//                   forecast.numMonth = d.getMonth();
+//                   forecast.spot_id = spot_ids[j];
+//                   forecast.spot_name = spotName(spot_ids[j]);
+//                   forecast.date = new Date(el.timestamp * 1000);
+//                   forecast.save(function(err){
+//                   if(err){
+//                     console.log("ERROR IN SAVING!",err);
+//                   }else{
+//                     console.log("Succesfully SAVED");
+//                   }
+//                 }); //end Save
+//                 console.log("SAVED");
+//             }else{
+//               console.log("IN THE DB!!!  SKIPPED!")
+//             }
+//            }); //end findOne check
+//          }) // end responseForEach
+//        })  // END API REQUEST
+//       })(i);  // end j
+//      }  // end For-loop    
+// }, null, true, 'America/Los_Angeles');   //end cron-job
 
 
 app.use(morgan("tiny"));
