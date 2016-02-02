5cf49e184017a8191f43d14237bdc81809fe523b
Mark Marsella <mmarsella114@g.rwu.edu>
2016-02-02 12:30:14 -0800

    switch to heroku auth

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -87,10 +87,6 @@ function spotName(spot_id){
 
 
 
-
-
-
-
 /********** PIE CHART *********************************************/
   // loop through all of the logs in Calendar, assign locations to the data []
   
@@ -166,12 +162,12 @@ function spotName(spot_id){
 
   
   // Bar Chart
-  $scope.labels3 = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data3 = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+  // $scope.labels3 = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  // $scope.series = ['Series A', 'Series B'];
+  // $scope.data3 = [
+  //   [65, 59, 80, 81, 56, 55, 40],
+  //   [28, 48, 40, 19, 86, 27, 90]
+  // ];
 
 
 /***********************************************/
